ed42110179961af8804196357111b0cdfa5a3aa8
sergiohdz977 <sergiohdz977@gmail.com>
2026-09-01 13:10:00 -0400

    ...

--- core/settings.py
+++ core/settings.py
@@ -141,6 +141,9 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
 from datetime import timedelta
 
