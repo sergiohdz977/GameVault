db234bb1e8c12cd7129ee4887b6484a0061d4086
sergiohdz977 <sergiohdz977@gmail.com>
2026-09-02 08:31:22 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('api/', include('games.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
-    path('api/schema/', SpectacularAPIView.as_view(),name = 'schema')
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema')
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
